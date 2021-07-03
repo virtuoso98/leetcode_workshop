bdd8f7cc7bc83f86d3a28f926948d50a25690621
virtuoso98 <58349087+virtuoso98@users.noreply.github.com>
2021-07-03 22:33:57 +0800

    first qn done

---
+++ Week1_Introduction/single-number.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        /* Create Frequency Table */
+        HashMap<Integer, Integer> freq_table = new HashMap<>();
+
+        /*
+         * - Loop through the Array. If value does not exist in HashMap, - then set
+         * default to 1. If not, add 1 to existing value
+         */
+        for (int i : nums) {
+            freq_table.put(i, freq_table.getOrDefault(i, 0) + 1);
+        }
+
+        /* Find the number that only occurs once in Frequency table */
+        for (int j : nums) {
+            if (freq_table.get(j) == 1) {
+                return j;
+            }
+        }
+        return 0;
+    }
+}
---
+++ Week1_Introduction/single-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        """
+        Finds the number that only occurs once
+        in nums, where every other element 
+        occurs twice.
+        """
+        
+        # Make frequency table
+        freq_table = {}
+        
+        # Create Tally. Very important trick
+        for i in nums:
+            if i in freq_table:
+                freq_table[i] += 1
+            else:
+                freq_table[i] = 1
+        
+        # Find number that only occurs once.
+        for num, freq in freq_table.items():
+            if freq == 1:
+                return num
+        
